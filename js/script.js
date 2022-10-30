e9b27d9a1b44cff2d386c368c31e310e6240818b
Use your GitHub username <Enter your email address>
2022-10-29 22:04:55 -0600

    Last Modification

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const today = new Date();
+document.querySelector("footer div span").textContent = today.getFullYear();
+document.getElementById("lastupdatedate").textContent = document.lastModified;
