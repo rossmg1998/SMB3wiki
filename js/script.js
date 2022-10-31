5d2860c957ea3381545fd97cf7f56547f3404dd5
Use your GitHub username <Enter your email address>
2022-10-30 19:48:33 -0600

    Added World 1 Level Statistics

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+// Hamburger Button
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn")
+x.onclick = toggleMenu;
+
+// Last Modified Date
 const today = new Date();
 document.querySelector("footer div span").textContent = today.getFullYear();
 document.getElementById("lastupdatedate").textContent = document.lastModified;
